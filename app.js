25c9c84f5d83a65e5cbfe435d12dc3e5b3828816
Jeremy Dowell <jdowell@fossil.com>
2013-04-11 10:11:49 -0500

    SQLITE Implementation

--- app.js
+++ app.js
@@ -13,7 +13,9 @@ var express = require('express')
   , radium = new EventEmitter()
   , fs  = require("fs")
   , sys = require('sys')
-  , exec = require('child_process').exec;
+  , exec = require('child_process').exec
+  , sqlite = require('sqlite3')
+  , clc = require('cli-color');
 
 
 var shouldCreateNewObject = true;
@@ -50,120 +52,90 @@ http.createServer(app).listen(app.get('port'), function(){
 
 
 /*** INITIAL PARSING ***/
-//if (path.existsSync("db/parser.db")) // or fs.existsSync
-//define db
-var db = require('sqlite-wrapper')('db/parser.db'); 
+//if the database doesn't exist, parse everything
 
-//prime the database
-db.createTable('user_table',{
-	'id': {
-		type: 'INTEGER',
-		primary: true,
-		notnull: true
-	},
-	'ip': {
-		type: 'TEXT'
-	},
-	'agent': {
-		type: 'TEXT'
-	},
-	'url': {
-		type: 'TEXT'
-	},
-	'referrer': {
-		type: 'TEXT'
-	},
-	'mobile': {
-		type: 'INTEGER'
-	}
-});
+if (!path.existsSync("db/parser.db")) { 
+	//clean the database
 
-db.createTable('data_table', {
-	'user_ip': {
-		type: 'TEXT'
-	},
-	'keyword': {
-		type: 'TEXT'
-	}
-});
+	//records arrays to insert into the db
+	var userRecords = new Array();
+	var dataRecords = new Array();
+	//keywords to exclude
+	var excludedKeywords = ['http','com','net','www','html','php','iframepull','iframe','stitial','section','adframe']
 
-//clean the database
-//exec("sqlite3 parser.db 'drop table data_table; drop table user_table;' && sqlite3 parser.db '.read parser.sql'");
+	//setup event listeners
+	radium.on('convertDone', defineData);
+	radium.on('dataDone', insertRecords);
 
-//records array to insert into the db
-var userRecords = new Array();
-var dataRecords = new Array();
-var excludedKeywords = ['http','com','net','www','html','php','iframepull','iframe','stitial','section','adframe']
+	console.time('total-parse-time');
+	console.time('load-file');
 
-//setup event listeners
-radium.on('convertDone', defineData);
-radium.on('dataDone', insertRecords);
+	fs.readFile('data/parsed_data.txt', { encoding: 'utf8'}, function(err,contents) {
+		console.timeEnd('load-file');
+		if (!err) {
+			
+			/**
+			* Data Cleaning Regular Expressions
+			* Match Blank Field -> /^\w*: (?=\n)/ ( and replace with '--') 
+			* Match label and space -> /^\w*: / ( and replace with '')
+			* Match eol -> /\n/  (and replace with 3 commas comma)
+			* Match ,,,****,****, -> /,\**,\**,/ (and replace with new line)
+			* Match ****,,, or ,,,**** -> \*{4},,,|,,,\*{4} (and replace with '')
+			**/
+			console.time('convert-file');
+			var parsed = contents.replace(/^\w*: (?=\n)/gm,'--').replace(/^\w*: /gm,'').replace(/\n/gm,',,,').replace(/,\**,\**,,,/g,'\n').replace(/\*{4},,,|,,,\*{4}/g,'');
+			var lines = parsed.split('\n');
+			console.log('total lines found: ' + lines.length);
+			fs.writeFile('data/parsed.txt', parsed );
+			lines.forEach(function(line,index,array) {
+				var parts = line.split(',,,');
 
-console.time('parse');
-console.time('load-file');
-fs.readFile('data/parsed_data.txt', { encoding: 'utf8'}, function(err,contents) {
-	console.timeEnd('load-file');
-	if (!err) {
-		//console.log(contents.replace(/\n/g,',' ));
-		
-		/**
-		* Data Cleaning Regular Expressions
-		* Match Blank Field -> /^\w*: (?=\n)/ ( and replace with '--')
-		* Match label and space -> /^\w*: / ( and replace with '')
-		* Match eol -> /\n/  (and replace with comma)
-		* Match ,****,****, -> /,\**,\**,/ (and replace with new line)
-		* Match ****, or ,**** -> \*{4},|,\*{4} (and replace with '')
-		**/
-		console.time('convert-file');
-		var parsed = contents.replace(/^\w*: (?=\n)/gm,'--').replace(/^\w*: /gm,'').replace(/\n/gm,',,,').replace(/,\**,\**,,,/g,'\n').replace(/\*{4},,,|,,,\*{4}/g,'');
-		var lines = parsed.split('\n');
-		console.log('total lines found: ' + lines.length);
-		fs.writeFile('data/parsed.txt', parsed );
-		lines.forEach(function(line,index,array) {
-			var parts = line.split(',,,');
+				if (parts[1]) {
+					var mobileMatches = parts[1].match(/mobile/g);
 
-			if (parts[1]) {
-				var mobileMatches = parts[1].match(/mobile/g);
-
-				var mobile = mobile ? true : false;
-				var record = {
-					ip: parts[0],
-					agent: parts[1],
-					url: parts[2],
-					referrer: parts[3],
-					mobile: mobile
-				};
-				userRecords.push(record);
-			}
-		
-			if (index == array.length -1) {
-				console.timeEnd('convert-file');
-				radium.emit('convertDone');
-			}
-		});
-	}
-});
-
+					var mobile = mobile ? true : false;
+					var record = {
+						$ip: parts[0],
+						$agent: parts[1],
+						$url: parts[2],
+						$referrer: parts[3],
+						$mobile: mobile
+					};
+					userRecords.push(record);
+				}
+			
+				if (index == array.length -1) {
+					console.timeEnd('convert-file');
+					radium.emit('convertDone');
+				}
+			});
+		} else {
+			console.log(clc.redBright.bold('ERROR LOADING FILE: ') + error);
+		}
+	});
+}
 
 function defineData() {
-	console.time('data-parse');
+	console.time('keyword-parse');
 	userRecords.forEach(function(record,index, array) {
-		//match any group of letters (with 3 ore more characters) that follow either a slash/dash/underscore/or dot
-		var matches = record.url.match(/(?=\/|-|_|)[a-z]{3,}/ig); 
+		//match any group of letters (with 3 ore more characters) that follow either a slash/dash/underscore
+		var matches = record.$url.match(/(?=\/|-|_|)[a-z]{3,}/ig); 
 		if (matches) {
 			matches.forEach(function(match) {
 				if(excludedKeywords.indexOf(match) == -1){
 					//console.log(match);
 					var dataRecord = {
-						user_ip: record.ip,
-						keyword: match
+						$user_ip: record.$ip,
+						$keyword: match
 					};
 					dataRecords.push(dataRecord);
 				}
 			});
 		}
 		if (index == array.length -1) {
-			console.timeEnd('data-parse');
+			console.timeEnd('keyword-parse');
+			console.log('keywords found: ' + dataRecords.length);
+			console.timeEnd('total-parse-time');
 			radium.emit('dataDone');
 		}
 	});
@@ -172,18 +144,66 @@ function defineData() {
 
 
 function insertRecords() {
-	console.log('keywords found: ' + dataRecords.length);
+
+	var sqlite3 = require('sqlite3').verbose(); 
+	var db = new sqlite3.cached.Database('data/parser.db');
+	var percentTitle = 'User Insertion: ';
+	var lastPercent = 0;
+	var userLength = userRecords.length;
+	var dataLength = dataRecords.length;
 	
-	console.time('user-db-write');
-	userRecords.forEach(function(record,index,array) {
-		db.insert('user_table',record)
-		if (index == array.length -1 )
-			console.timeEnd('user-db-write');
+	db.serialize(function() {
+		console.log('creating keyword table....');
+		db.run('CREATE TABLE "data_table" ("user_ip" INTEGER NOT NULL,"keyword" TEXT NOT NULL);', function(err) {
+			console.log('data_table created');
+		});
+		console.log('creating user table....');
+		db.run('CREATE TABLE "user_table" ("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,"ip" TEXT,"agent" TEXT,"url" TEXT,"referrer" TEXT,"mobile" INTEGER DEFAULT 0);', function(err) {
+			console.log('user_table created');
+		})
+		db.parallelize(function() {
+			console.log('inserting data...');
+			
+			console.time('user-db-write');
+			userRecords.forEach(function(record,index,array) {
+				db.run('INSERT into user_table(ip,agent,url, referrer, mobile) values($ip,$agent,$url,$referrer,$mobile)',record, function(err) {
+					if (err) {
+						console.log(clc.redBright.bold('ERROR INSERTING INTO USER TABLE: ') + err);
+					} else {
+						if (index == array.length -1 ) {
+							console.timeEnd('user-db-write');	
+							percent = 100;
+						} else {
+							percent = Math.floor((index / userLength)*100);
+						}
+						if (percent != lastPercent ) {
+							lastPercent = percent;
+							console.log(percentTitle + lastPercent + '%');
+						}
+					}
+				});
+			});
+			
+			percentTitle = "Data Insertion: ";
+			console.time('data-db-write');
+			dataRecords.forEach(function(record,index,array) {
+				db.run('INSERT into data_table(user_ip,keyword) values($user_ip,$keyword)',record, function(err) {
+					if (err) {
+						console.log(clc.redBright.bold('ERROR INSERTING INTO KEYWORD TABLE: ') + err);
+					} else {
+						if (index == array.length -1 ) {
+							console.timeEnd('data-db-write');	
+							percent = 100;
+						} else {
+							percent = Math.floor((index / dataLength)*100);
+						}
+						if (percent != lastPercent ) {
+							lastPercent = percent;
+							console.log(percentTitle + lastPercent + '%');
+						}
+					}
+				});
+			});
+		});
 	});
-	
-	console.time('data-db-write');
-	db.insertAll('data_table',dataRecords, function() {
-		console.timeEnd('data-db-write');
-	});
-	
 }
